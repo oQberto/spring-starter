931991364c23a386563e591feddebec49bc0c2ba
Ermakov Alexandr <ermakov.sascha2015yandex.ru@gmail.com>
2023-09-24 11:38:43 +0300

    learn: learn @Condition annotation

---
+++ src/main/java/org/example/spring/config/JpaConfiguration.java
@@ -0,0 +1,16 @@
+package org.example.spring.config;
+
+import jakarta.annotation.PostConstruct;
+import org.example.spring.config.condition.JpaCondition;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.Configuration;
+
+@Conditional(JpaCondition.class)
+@Configuration
+public class JpaConfiguration {
+
+    @PostConstruct
+    void init() {
+        System.out.println("Jpa configuration is enable");
+    }
+}
