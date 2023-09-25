3ebac86afc15d3e8bd505838c4588efb60365a51
Ermakov Alexandr <ermakov.sascha2015yandex.ru@gmail.com>
2023-09-25 12:12:11 +0300

    learn: learn how to connect and write unit tests using spring-boot-starter-test

---
+++ src/test/java/org/example/spring/service/CompanyServiceTest.java
@@ -0,0 +1,46 @@
+package org.example.spring.service;
+
+import org.example.spring.database.entity.Company;
+import org.example.spring.database.repository.CrudRepository;
+import org.example.spring.dto.CompanyReadDto;
+import org.example.spring.listener.entity.EntityEvent;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.context.ApplicationEventPublisher;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class CompanyServiceTest {
+    private static final Integer COMPANY_ID = 1;
+
+    @Mock
+    private UserService userService;
+    @Mock
+    private CrudRepository<Integer, Company> companyRepository;
+    @Mock
+    private ApplicationEventPublisher applicationEventPublisher;
+    @InjectMocks
+    private CompanyService companyService;
+
+    @Test
+    void findById() {
+        when(companyRepository.findById(COMPANY_ID)).thenReturn(Optional.of(new Company(COMPANY_ID)));
+
+        Optional<CompanyReadDto> actualResult = companyService.findById(COMPANY_ID);
+        assertTrue(actualResult.isPresent());
+
+        CompanyReadDto expectedResult = new CompanyReadDto(COMPANY_ID);
+        actualResult.ifPresent(actual -> assertEquals(expectedResult, actualResult.get()));
+
+        verify(applicationEventPublisher).publishEvent(any(EntityEvent.class));
+        verifyNoMoreInteractions(applicationEventPublisher, userService);
+    }
+}
