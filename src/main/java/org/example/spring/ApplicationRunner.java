cdaae13153df7aedeba73f3d96b29d0391374405
Ermakov Alexandr <ermakov.sascha2015yandex.ru@gmail.com>
2023-09-25 21:11:30 +0300

    docs: add explanations to annotations

--- src/main/java/org/example/spring/ApplicationRunner.java
+++ src/main/java/org/example/spring/ApplicationRunner.java
@@ -6,6 +6,38 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.ConfigurableApplicationContext;
 
+/**
+ * {@code @SpringBootApplication} - это мощная аннотация в Spring Boot, которая объединяет несколько других аннотаций
+ * для упрощения конфигурации и запуска приложений на платформе Spring. Она является аннотацией мета-конфигурации,
+ * которая объединяет следующие аннотации:
+ * <p>
+ * {@code @Configuration:} Обозначает класс как конфигурационный и позволяет определить бины в приложении.
+ * <p>
+ * {@code @EnableAutoConfiguration:} Автоматически настраивает Spring-приложение на основе классов в пути classpath и
+ * настройках, что позволяет создавать приложения с минимальными усилиями по конфигурации.
+ * <p>
+ * {@code @ComponentScan:} Сканирует и регистрирует компоненты (классы, помеченные @Component, @Service, @Repository,
+ * и др.) в контексте приложения.
+ * <p>
+ * {@code @SpringBootApplication} используется как аннотация верхнего уровня для вашего главного класса
+ * (класса, содержащего метод main)
+ * <p></p>
+ *{@code @ConfigurationPropertiesScan} - это аннотация в Spring Boot, введенная в версии Spring Boot 2.2, которая используется
+ *  для сканирования пакетов и обнаружения классов, помеченных аннотацией @ConfigurationProperties. Это удобное средство,
+ *  которое позволяет автоматически обнаруживать классы, предназначенные для привязки внешних настроек к Java-объектам (POJOs).
+ * <p>
+ * Основные характеристики и использование @ConfigurationPropertiesScan:
+ * <p>
+ * Сканирование пакетов: @ConfigurationPropertiesScan сканирует указанные пакеты в поисках классов, помеченных аннотацией
+ * {@code @ConfigurationProperties,} и регистрирует их в контексте Spring. Это позволяет Spring Boot автоматически преобразовывать
+ * внешние конфигурации в Java-объекты.
+ * <p>
+ * Простое подключение настроек: При использовании @ConfigurationProperties вы можете создать POJO для хранения
+ * конфигурационных параметров вашего приложения и автоматически привязать значения из application.properties или
+ * application.yml файлов к полям этого класса.
+ * <p>
+ * Явная конфигурация пакетов: Вы можете явно указать, какие пакеты должны быть отсканированы с помощью @ConfigurationPropertiesScan, либо дать ему сканировать пакет, в котором находится сама аннотация.
+ */
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class ApplicationRunner {
--- src/main/java/org/example/spring/config/ApplicationConfiguration.java
+++ src/main/java/org/example/spring/config/ApplicationConfiguration.java
@@ -12,20 +12,61 @@ import static org.springframework.beans.factory.config.BeanDefinition.SCOPE_SING
 @Configuration
 public class ApplicationConfiguration {
 
-        @Bean
-        @Scope(SCOPE_SINGLETON)
-        public ConnectionPool connectionPool1() {
-                return new ConnectionPool("test-name", 20);
-        }
+    /**
+     * {@code @Bean} - это аннотация в Spring Framework, которая используется для определения методов, которые создают
+     * и настраивают бины в Spring контейнере. Методы, помеченные @Bean, предоставляют конфигурацию для создания
+     * и настройки компонентов в приложении. Эти методы обычно определены в классах, помеченных
+     * аннотацией @Configuration.
+     * <p>
+     * Основные характеристики и использование аннотации @Bean:
+     * <p>
+     * Создание бинов: Методы, помеченные @Bean, создают и настраивают бины. Это позволяет полностью
+     * контролировать процесс создания объектов в Spring контейнере.
+     * <p>
+     * Именование бинов: Методы, помеченные @Bean, возвращают объекты, которые становятся бинами Spring.
+     * Вы можете использовать атрибут name аннотации для явного задания имени бина. Если имя не указано,
+     * оно будет автоматически сгенерировано на основе имени метода.
+     * <p>
+     * Зависимости и конфигурация: Методы @Bean могут иметь параметры, которые внедряются из контекста Spring.
+     * Это позволяет настраивать бины с учетом их зависимостей.
+     * <p>
+     * Профили: Вы можете использовать аннотацию @Profile вместе с @Bean, чтобы определить,
+     * в каких профилях приложения данный бин должен быть создан.
+     * <p></p>
+     * {@code @Scope} - это аннотация в Spring Framework, которая используется для определения области (scope) бина, т.е.
+     * времени его жизни и доступности в контейнере Spring. С помощью @Scope вы можете указать, как долго бин должен
+     * существовать и каким образом он должен быть доступен в рамках приложения.
+     * <p>
+     * Spring предоставляет несколько стандартных областей (scope), таких, как singleton, prototype, request, session и
+     * другие. Вы также можете создавать свои собственные пользовательские области.
+     * <p>
+     * Основные характеристики и использование аннотации @Scope:
+     * <p>
+     * Стандартные области: Spring предоставляет несколько стандартных областей, которые определяют, как долго бин будет
+     * существовать. Например, singleton означает, что бин будет создан только один раз и будет использоваться во всем
+     * приложении, а prototype означает, что будет создан новый экземпляр бина каждый раз, когда он запрашивается.
+     * <p>
+     * Свои собственные области: Вы можете создавать собственные пользовательские области, реализуя интерфейс
+     * org.springframework.beans.factory.config. Scope и регистрируя их в контейнере Spring. Это позволяет настраивать
+     * более сложные сценарии управления временем жизни бинов.
+     * <p>
+     * Использование аннотации @Scope: Для применения @Scope к бину, просто пометьте класс или метод, который определяет
+     * бин, этой аннотацией и укажите имя или значение стандартной, или пользовательской области.
+     */
+    @Bean
+    @Scope(SCOPE_SINGLETON)
+    public ConnectionPool connectionPool1() {
+        return new ConnectionPool("test-name", 20);
+    }
 
-        @Bean
-        @Scope(SCOPE_SINGLETON)
-        public ConnectionPool connectionPool2() {
-                return new ConnectionPool("test-name", 20);
-        }
+    @Bean
+    @Scope(SCOPE_SINGLETON)
+    public ConnectionPool connectionPool2() {
+        return new ConnectionPool("test-name", 20);
+    }
 
-        @Bean
-        public UserRepository userRepository1(ConnectionPool connectionPool) {
-                return new UserRepository(connectionPool);
-        }
+    @Bean
+    public UserRepository userRepository1(ConnectionPool connectionPool) {
+        return new UserRepository(connectionPool);
+    }
 }
