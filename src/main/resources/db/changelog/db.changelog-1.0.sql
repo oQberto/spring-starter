5e61c1477380298932013c30ebe1e18cfdce8b07
Ermakov Alexandr <ermakov.sascha2015yandex.ru@gmail.com>
2023-10-01 17:23:28 +0300

    learn: learn how to use migration framework liquibase in the spring applications

--- src/main/resources/db/changelog/db.changelog-1.0.sql
+++ src/main/resources/db/changelog/db.changelog-1.0.sql
@@ -1,9 +1,13 @@
+--liquibase formatted sql
+
+--changeset alexermakov:1
 CREATE TABLE IF NOT EXISTS company
 (
     id SERIAL PRIMARY KEY ,
     name VARCHAR(64) NOT NULL UNIQUE
     );
 
+--changeset alexermakov:2
 CREATE TABLE IF NOT EXISTS company_locales
 (
     company_id INT REFERENCES company (id),
@@ -12,6 +16,8 @@ CREATE TABLE IF NOT EXISTS company_locales
     PRIMARY KEY (company_id, lang)
     );
 
+
+--changeset alexermakov:3
 CREATE TABLE IF NOT EXISTS users
 (
     id BIGSERIAL PRIMARY KEY ,
@@ -23,6 +29,7 @@ CREATE TABLE IF NOT EXISTS users
     company_id INT REFERENCES company (id)
     );
 
+--changeset alexermakov:4
 CREATE TABLE IF NOT EXISTS payment
 (
     id BIGSERIAL PRIMARY KEY ,
@@ -30,12 +37,14 @@ CREATE TABLE IF NOT EXISTS payment
     receiver_id BIGINT NOT NULL REFERENCES users (id)
     );
 
+--changeset alexermakov:5
 CREATE TABLE IF NOT EXISTS chat
 (
     id BIGSERIAL PRIMARY KEY ,
     name VARCHAR(64) NOT NULL UNIQUE
     );
 
+--changeset alexermakov:6
 CREATE TABLE IF NOT EXISTS users_chat
 (
     id BIGSERIAL PRIMARY KEY ,
--- src/main/resources/db/changelog/db.changelog-2.0.sql
+++ src/main/resources/db/changelog/db.changelog-2.0.sql
@@ -1,3 +1,6 @@
+--liquibase formatted sql
+
+--changeset alexermakov:1
 ALTER TABLE users
     ADD COLUMN created_at TIMESTAMP;
 
