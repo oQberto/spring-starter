c3a8a66bab0b63328b944dc473604669fe345ea8
Ermakov Alexandr <ermakov.sascha2015yandex.ru@gmail.com>
2023-10-02 14:10:01 +0300

    learn: learn how to get and set attributes into session/request scope

---
+++ src/main/java/org/example/spring/dto/UserReadDto.java
@@ -0,0 +1,9 @@
+package org.example.spring.dto;
+
+import lombok.Value;
+
+@Value
+public class UserReadDto {
+    Long id;
+    String username;
+}
--- src/main/java/org/example/spring/http/controller/GreetingController.java
+++ src/main/java/org/example/spring/http/controller/GreetingController.java
@@ -1,20 +1,31 @@
 package org.example.spring.http.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.example.spring.dto.UserReadDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/api/v1")
+@SessionAttributes({"user"})
 public class GreetingController {
 
-    //    @RequestMapping(
+    @GetMapping(value = "/hello")
+    public ModelAndView hello(ModelAndView modelAndView){
+        modelAndView.setViewName("greeting/hello");
+
+        // установка атрибута
+        modelAndView.addObject("user", new UserReadDto(1L, "username"));
+        return modelAndView;
+    }
+
+//        @RequestMapping(
 //            value = "/hello",
 //            method = RequestMethod.GET
 //    )
     @GetMapping(value = "/hello/{id}")
-    public ModelAndView hello(ModelAndView modelAndView, HttpServletRequest request,
+    public ModelAndView hello2(ModelAndView modelAndView, HttpServletRequest request,
                               @RequestParam("age") Integer age,
                               @RequestHeader("accept") String accept,
                               @CookieValue("JSESSIONID") String jsessionId,
@@ -24,12 +35,13 @@ public class GreetingController {
         return modelAndView;
     }
 
-    //    @RequestMapping(
+//        @RequestMapping(
 //            value = "/bye",
 //            method = RequestMethod.GET
 //    )
     @GetMapping(value = "/bye")
-    public ModelAndView bye(ModelAndView modelAndView) {
+    public ModelAndView bye(ModelAndView modelAndView,
+                            @SessionAttribute("user") UserReadDto user) {
         modelAndView.setViewName("greeting/bye");
 
         return modelAndView;
