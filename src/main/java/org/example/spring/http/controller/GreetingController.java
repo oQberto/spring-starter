b9a091f137afb5c555196f72a284e48bb3f84342
Ermakov Alexandr <ermakov.sascha2015yandex.ru@gmail.com>
2023-10-03 14:39:12 +0300

    learn: learn how to work with thymeleaf; docs: add an explanation to @SessionAttributes & @SessionAttribute annotations in the GreetingController

--- src/main/java/org/example/spring/http/controller/GreetingController.java
+++ src/main/java/org/example/spring/http/controller/GreetingController.java
@@ -11,6 +11,20 @@ import org.springframework.web.servlet.ModelAndView;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * {@code @SessionAttributes} - это аннотация, которая используется для пометки класса контроллера и определения
+ * атрибутов, которые должны сохраняться в сессии пользователя.
+ * <p>
+ * Обычно @SessionAttributes используется на уровне класса контроллера и принимает в качестве аргумента массив строк,
+ * представляющих имена атрибутов, которые должны храниться в сессии.
+ * <p>
+ * Атрибуты, перечисленные в @SessionAttributes, будут автоматически сохраняться в сессии при выполнении методов
+ * контроллера, а также будут доступны для чтения и изменения в других методах контроллера.
+ * <p></p>
+ * Обе аннотации @SessionAttributes и @SessionAttribute полезны при работе с данными, которые должны быть доступны
+ * в течение всей сессии пользователя, например, информацией о текущем пользователе, состоянием корзины покупок и
+ * другими данными, которые должны сохраняться между запросами клиента.
+ */
 @Controller
 @RequestMapping("/api/v1")
 @SessionAttributes({"user"})
@@ -18,6 +32,7 @@ public class GreetingController {
 
     /**
      * Метод будет вызываться каждый раз, на каждый запрос.
+     *
      * @return
      */
     @ModelAttribute("roles")
@@ -27,23 +42,23 @@ public class GreetingController {
 
     @GetMapping(value = "/hello")
     public String hello(Model model,
-                        @ModelAttribute("userReadDto") UserReadDto userReadDto){
+                        @ModelAttribute("userReadDto") UserReadDto userReadDto) {
 
         // установка атрибута
-//        model.addAttribute("user", new UserReadDto(1L, "username"));
+        model.addAttribute("user", userReadDto);
         return "greeting/hello";
     }
 
-//        @RequestMapping(
+    //        @RequestMapping(
 //            value = "/hello",
 //            method = RequestMethod.GET
 //    )
     @GetMapping(value = "/hello/{id}")
     public ModelAndView hello2(ModelAndView modelAndView, HttpServletRequest request,
-                              @RequestParam("age") Integer age,
-                              @RequestHeader("accept") String accept,
-                              @CookieValue("JSESSIONID") String jsessionId,
-                              @PathVariable("id") Integer id) {
+                               @RequestParam("age") Integer age,
+                               @RequestHeader("accept") String accept,
+                               @CookieValue("JSESSIONID") String jsessionId,
+                               @PathVariable("id") Integer id) {
         modelAndView.setViewName("greeting/hello");
 
         return modelAndView;
@@ -51,6 +66,15 @@ public class GreetingController {
 
     /**
      * Если нам надо вернуть просто статичную страницу, то вместо ModelAndView мы можем вернуть String.
+     * <p>
+     * {@code @SessionAttribute} - это аннотация, которая используется для извлечения значения атрибута из сессии в
+     * методе контроллера.
+     * <p>
+     * Она применяется к параметрам метода и указывает имя атрибута, который должен быть извлечен из сессии и передан
+     * в метод контроллера.
+     * <p>
+     * Эта аннотация полезна, когда вам нужно получить доступ к конкретному атрибуту сессии внутри метода контроллера.
+     *
      * @param modelAndView
      * @param user
      * @return
@@ -61,7 +85,7 @@ public class GreetingController {
 //    )
     @GetMapping(value = "/bye")
     public String bye(ModelAndView modelAndView,
-                            @SessionAttribute("user") UserReadDto user) {
+                      @SessionAttribute("user") UserReadDto user) {
         return "greeting/bye";
     }
 }
