278ca0532b9b576a954d25bcfad8cc9fe4bf62f2
Ermakov Alexandr <ermakov.sascha2015yandex.ru@gmail.com>
2023-10-15 09:21:52 +0300

    learn: learn csfr filter

--- src/main/java/org/example/spring/config/SecurityConfiguration.java
+++ src/main/java/org/example/spring/config/SecurityConfiguration.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -29,7 +28,6 @@ public class SecurityConfiguration {
                         .requestMatchers("/users/\\d+/remove").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
-                .csrf(AbstractHttpConfigurer::disable)
                 .logout(logout -> logout
                         .logoutUrl("/logout")
                         .logoutSuccessUrl("/login")
